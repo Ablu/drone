8e545f6a1abd59fcf78aa66a2d5b21e51c0179cd
Erik Schilling <ablu.erikschilling@gmail.com>
2016-07-12 14:27:26 +0200

    install wget too

--- build.sh
+++ build.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-wget https://storage.googleapis.com/golang/go1.6.2.linux-amd64.tar.gz
-sudo tar -C /usr/local -xzf go1.6.2.linux-amd64.tar.gz
-export PATH=$PATH:/usr/local/go/bin
+yum install -y sqlite-devel make wget || exit 1
 
-yum install -y sqlite-devel make
+wget https://storage.googleapis.com/golang/go1.6.2.linux-amd64.tar.gz || exit 1
+sudo tar -C /usr/local -xzf go1.6.2.linux-amd64.tar.gz || exit 1
+export PATH=$PATH:/usr/local/go/bin || exit 1
 
-mkdir -p ~/go/src/github.com/drone/
-export GOPATH=~/go
-ln -s $(pwd) ~/go/src/github.com/drone/drone
+mkdir -p ~/go/src/github.com/drone/ || exit 1
+export GOPATH=~/go || exit 1
+ln -s $(pwd) ~/go/src/github.com/drone/drone || exit 1
 
 export GO15VENDOREXPERIMENT=1
-make deps
-make gen
-make build
+make deps || exit 1
+make gen || exit 1
+make build || exit 1
 
 
 
